34ab3f9fb75ef274a7ea3ccf6e922ee9ac0b3651
Vishesh <vishesht27@gmail.com>
2023-07-23 16:22:53 +0530

    Added and updated the connector

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
+# streamlit_app.py
+
 import streamlit as st
 
-st.set_page_config(
-    page_title="Streamlit Connectors",
-    page_icon="🧊",
-)
+# Initialize connection.
+conn = st.experimental_connection('mysql', type='sql')
 
-st.title("🧊 Streamlit Connector")
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
+
+# import streamlit as st
+
+# conn = st.experimental_connection('pets', type='sql')
+# pet_owners = conn.query('select * from mytable;')
+# st.dataframe(pet_owners)
+
+
+import streamlit as st
+import mysql.connector
+
+@st.cache_resource
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from pet_owners;")
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
